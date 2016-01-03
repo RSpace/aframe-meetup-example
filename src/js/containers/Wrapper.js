3683b70d14fab022c739752b5596d71db57373fe
Casper Fabricius <me@casperfabricius.com>
2016-01-03 11:37:25 +0100

    Lets remove Light and Sky, not needed for this demo

--- src/js/containers/Wrapper.js
+++ src/js/containers/Wrapper.js
@@ -1,13 +1,11 @@
 import React, { Component, PropTypes } from 'react'
-import DevTools from './DevTools'
+//import DevTools from './DevTools'
 import 'aframe-core'
 import {Animation, Entity, Scene} from 'aframe-react'
 import { connect } from 'react-redux'
 
 import Camera from '../components/Camera'
 import Cursor from '../components/Cursor'
-import Light from '../components/Light'
-import Sky from '../components/Sky'
 import Members from './Members'
 import VRModeSwitcher from './VRModeSwitcher'
 
@@ -20,18 +18,9 @@ class Wrapper extends Component {
         <div>
           <Scene append-canvas="true">
             <Camera><Cursor/></Camera>
-
-            <Sky/>
-
-            <Light type="ambient" color="#888"/>
-            <Light type="directional" intensity="0.5" position="-1 1 0"/>
-            <Light type="directional" intensity="1" position="1 1 0"/>
-
             <Members/>
-
             <VRModeSwitcher/>
           </Scene>
-
         </div>
       )
     } else {
