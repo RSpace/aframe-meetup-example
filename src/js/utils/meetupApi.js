c7bbb867291ce4649e4b5325733e2737739b39b7
Casper Fabricius <me@casperfabricius.com>
2017-03-19 21:59:06 +0100

    Updated API call to new API version and using members from the CPHJS meetup

--- src/js/utils/meetupApi.js
+++ src/js/utils/meetupApi.js
@@ -2,10 +2,10 @@ import fetchJsonp from 'fetch-jsonp'
 import {List, Map} from 'immutable'
 
 const API_ROOT = 'https://api.meetup.com'
-const GET_MEMBERS_URL = API_ROOT + '/2/rsvps?offset=0&format=json&event_id=228082564&photo-host=public&page=100&fields=&order=event&desc=false&sig_id=45273132&sig=a75637600bca84224283fb5dc2afcb73bb5f8bee'
+const GET_MEMBERS_URL = API_ROOT + '/copenhagenjs/events/238301512/rsvps?photo-host=public&sig_id=45273132&sig=68eff7466160d0354c7ee6e791c16c55a7e9c2bf'
 
 export function getMembers() {
-  return fetchJsonp(GET_MEMBERS_URL)
+  return fetchJsonp(GET_MEMBERS_URL, {timeout: 10000})
     .then(response =>
       response.json().then(json => ({ json, response }))
     ).then(({ json, response }) => {
@@ -15,12 +15,12 @@ export function getMembers() {
 
       let members = []
 
-      for(let result of json.results) {
-        if(result.member_photo) {
+      for(let result of json.data) {
+        if(result.member.photo) {
           members.push(Map({
-            id: result.member.member_id,
+            id: result.member.id,
             name: result.member.name,
-            photo_url: result.member_photo.photo_link
+            photo_url: result.member.photo.photo_link
           }))
         }
       }
